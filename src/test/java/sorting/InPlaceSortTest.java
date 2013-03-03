a251bc78a1887cb9be6b6d837e74cc0eab236bcb
loic <loic@loic-desktop>
2013-03-03 17:18:49 +0100

    add generic test super class for in-place sorting algorithm

---
+++ src/test/java/sorting/InPlaceSortTest.java
@@ -0,0 +1,89 @@
+package sorting;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public abstract class InPlaceSortTest {
+
+    private InPlaceSort sortingAlgorithm;
+
+    @Before
+    public void setUp() {
+        sortingAlgorithm = createSortingAlgorithm();
+    }
+
+    protected abstract InPlaceSort createSortingAlgorithm();
+
+    @Test
+    public void testEmptyQuickSort() {
+        int[] expecteds = new int[0];
+        int[] actuals = new int[0];
+        sortingAlgorithm.sort(actuals);
+        Assert.assertArrayEquals(expecteds, actuals);
+    }
+
+    @Test
+    public void testQuickSortOnSingleElement() {
+        int[] expecteds = new int[] { 1 };
+        int[] actuals = new int[] { 1 };
+        sortingAlgorithm.sort(actuals);
+        Assert.assertArrayEquals(expecteds, actuals);
+    }
+
+    @Test
+    public void testQuickSortOnTwosortedElements() {
+        int[] expecteds = new int[] { 1, 2 };
+        int[] actuals = new int[] { 1, 2 };
+        sortingAlgorithm.sort(actuals);
+        Assert.assertArrayEquals(expecteds, actuals);
+    }
+
+    @Test
+    public void testQuickSortOnTwoUnsortedElements() {
+        int[] expecteds = new int[] { 1, 2 };
+        int[] actuals = new int[] { 2, 1 };
+        sortingAlgorithm.sort(actuals);
+        Assert.assertArrayEquals(expecteds, actuals);
+    }
+
+    @Test
+    public void testQuickSortOnManyElements1() {
+        int[] expecteds = new int[] { 1, 2, 3, 3, 4, 5, 8, 11 };
+        int[] actuals = new int[] { 3, 2, 11, 5, 1, 8, 3, 4 };
+        sortingAlgorithm.sort(actuals);
+        Assert.assertArrayEquals(expecteds, actuals);
+    }
+
+    @Test
+    public void testQuickSortOnManyElements2() {
+        int[] expecteds = new int[] { 1, 2, 3, 4, 5, 5, 7, 8, 9 };
+        int[] actuals = new int[] { 3, 7, 8, 5, 2, 1, 9, 5, 4 };
+        sortingAlgorithm.sort(actuals);
+        Assert.assertArrayEquals(expecteds, actuals);
+    }
+
+    @Test
+    public void testQuickSortOnManyElements3() {
+        int size = 10000;
+        int[] expecteds = new int[size];
+        for (int i = 0; i < size; i++) {
+            int value = (int) (Math.random() * Integer.MAX_VALUE * (Math.random() < 0.5 ? -1d : 1d));
+            expecteds[i] = value;
+        }
+        int[] actuals = Arrays.copyOf(expecteds, expecteds.length);
+        Arrays.sort(expecteds);
+        sortingAlgorithm.sort(actuals);
+        Assert.assertArrayEquals(expecteds, actuals);
+    }
+
+    @Test
+    public void testQuickSortOnManyElements4() {
+        int[] expecteds = new int[] { 2, 4, 5, 6, 7, 8, 9, 11, 12, 13, 19, 21 };
+        int[] actuals = new int[] { 13, 19, 9, 5, 12, 8, 7, 4, 11, 2, 6, 21 };
+        sortingAlgorithm.sort(actuals);
+        Assert.assertArrayEquals(expecteds, actuals);
+    }
+}
