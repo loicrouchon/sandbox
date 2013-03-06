e317c0c02f73b25de71523b1853e75faff5ed6aa
loic <loic@loic-desktop>
2013-03-06 22:02:52 +0100

    rename test methods

--- src/test/java/sorting/InPlaceSortAlgorithmTest.java
+++ src/test/java/sorting/InPlaceSortAlgorithmTest.java
@@ -18,7 +18,7 @@ public abstract class InPlaceSortAlgorithmTest {
     protected abstract InPlaceSortAlgorithm createSortingAlgorithm();
 
     @Test
-    public void testEmptyQuickSort() {
+    public void testEmptySort() {
         int[] expecteds = new int[0];
         int[] actuals = new int[0];
         sortAlgorithm.sort(actuals);
@@ -26,7 +26,7 @@ public abstract class InPlaceSortAlgorithmTest {
     }
 
     @Test
-    public void testQuickSortOnSingleElement() {
+    public void testSortOnSingleElement() {
         int[] expecteds = new int[] { 1 };
         int[] actuals = new int[] { 1 };
         sortAlgorithm.sort(actuals);
@@ -34,7 +34,7 @@ public abstract class InPlaceSortAlgorithmTest {
     }
 
     @Test
-    public void testQuickSortOnTwosortedElements() {
+    public void testSortOnTwosortedElements() {
         int[] expecteds = new int[] { 1, 2 };
         int[] actuals = new int[] { 1, 2 };
         sortAlgorithm.sort(actuals);
@@ -42,7 +42,7 @@ public abstract class InPlaceSortAlgorithmTest {
     }
 
     @Test
-    public void testQuickSortOnTwoUnsortedElements() {
+    public void testSortOnTwoUnsortedElements() {
         int[] expecteds = new int[] { 1, 2 };
         int[] actuals = new int[] { 2, 1 };
         sortAlgorithm.sort(actuals);
@@ -50,7 +50,7 @@ public abstract class InPlaceSortAlgorithmTest {
     }
 
     @Test
-    public void testQuickSortOnManyElements1() {
+    public void testSortOnManyElements1() {
         int[] expecteds = new int[] { 1, 2, 3, 3, 4, 5, 8, 11 };
         int[] actuals = new int[] { 3, 2, 11, 5, 1, 8, 3, 4 };
         sortAlgorithm.sort(actuals);
@@ -58,7 +58,7 @@ public abstract class InPlaceSortAlgorithmTest {
     }
 
     @Test
-    public void testQuickSortOnManyElements2() {
+    public void testSortOnManyElements2() {
         int[] expecteds = new int[] { 1, 2, 3, 4, 5, 5, 7, 8, 9 };
         int[] actuals = new int[] { 3, 7, 8, 5, 2, 1, 9, 5, 4 };
         sortAlgorithm.sort(actuals);
@@ -66,8 +66,8 @@ public abstract class InPlaceSortAlgorithmTest {
     }
 
     @Test
-    public void testQuickSortOnManyElements3() {
-        int size = 10000;
+    public void testSortOnManyElements3() {
+        int size = 100000;
         int[] expecteds = new int[size];
         for (int i = 0; i < size; i++) {
             int value = (int) (Math.random() * Integer.MAX_VALUE * (Math.random() < 0.5 ? -1d : 1d));
@@ -80,7 +80,7 @@ public abstract class InPlaceSortAlgorithmTest {
     }
 
     @Test
-    public void testQuickSortOnManyElements4() {
+    public void testSortOnManyElements4() {
         int[] expecteds = new int[] { 2, 4, 5, 6, 7, 8, 9, 11, 12, 13, 19, 21 };
         int[] actuals = new int[] { 13, 19, 9, 5, 12, 8, 7, 4, 11, 2, 6, 21 };
         sortAlgorithm.sort(actuals);
--- src/test/java/sorting/SortAlgorithmTest.java
+++ src/test/java/sorting/SortAlgorithmTest.java
@@ -18,7 +18,7 @@ public abstract class SortAlgorithmTest {
     protected abstract SortAlgorithm createSortingAlgorithm();
 
     @Test
-    public void testEmptyQuickSort() {
+    public void testEmptySort() {
         int[] expecteds = new int[0];
         int[] actuals = new int[0];
         actuals = sortAlgorithm.sort(actuals);
@@ -26,7 +26,7 @@ public abstract class SortAlgorithmTest {
     }
 
     @Test
-    public void testQuickSortOnSingleElement() {
+    public void testSortOnSingleElement() {
         int[] expecteds = new int[] { 1 };
         int[] actuals = new int[] { 1 };
         actuals = sortAlgorithm.sort(actuals);
@@ -34,7 +34,7 @@ public abstract class SortAlgorithmTest {
     }
 
     @Test
-    public void testQuickSortOnTwosortedElements() {
+    public void testSortOnTwosortedElements() {
         int[] expecteds = new int[] { 1, 2 };
         int[] actuals = new int[] { 1, 2 };
         actuals = sortAlgorithm.sort(actuals);
@@ -42,7 +42,7 @@ public abstract class SortAlgorithmTest {
     }
 
     @Test
-    public void testQuickSortOnTwoUnsortedElements() {
+    public void testSortOnTwoUnsortedElements() {
         int[] expecteds = new int[] { 1, 2 };
         int[] actuals = new int[] { 2, 1 };
         actuals = sortAlgorithm.sort(actuals);
@@ -50,7 +50,7 @@ public abstract class SortAlgorithmTest {
     }
 
     @Test
-    public void testQuickSortOnManyElements1() {
+    public void testSortOnManyElements1() {
         int[] expecteds = new int[] { 1, 2, 3, 3, 4, 5, 8, 11 };
         int[] actuals = new int[] { 3, 2, 11, 5, 1, 8, 3, 4 };
         actuals = sortAlgorithm.sort(actuals);
@@ -58,7 +58,7 @@ public abstract class SortAlgorithmTest {
     }
 
     @Test
-    public void testQuickSortOnManyElements2() {
+    public void testSortOnManyElements2() {
         int[] expecteds = new int[] { 1, 2, 3, 4, 5, 5, 7, 8, 9 };
         int[] actuals = new int[] { 3, 7, 8, 5, 2, 1, 9, 5, 4 };
         actuals = sortAlgorithm.sort(actuals);
@@ -66,7 +66,7 @@ public abstract class SortAlgorithmTest {
     }
 
     @Test
-    public void testQuickSortOnManyElements3() {
+    public void testSortOnManyElements3() {
         int size = 10000;
         int[] expecteds = new int[size];
         for (int i = 0; i < size; i++) {
@@ -80,7 +80,7 @@ public abstract class SortAlgorithmTest {
     }
 
     @Test
-    public void testQuickSortOnManyElements4() {
+    public void testSortOnManyElements4() {
         int[] expecteds = new int[] { 2, 4, 5, 6, 7, 8, 9, 11, 12, 13, 19, 21 };
         int[] actuals = new int[] { 13, 19, 9, 5, 12, 8, 7, 4, 11, 2, 6, 21 };
         actuals = sortAlgorithm.sort(actuals);
