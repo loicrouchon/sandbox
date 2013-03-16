5e58c899cd094aa9305903c24ca3f7f29cab8015
loic <loic@loic-desktop>
2013-03-16 17:46:48 +0100

    factorize SortTest

--- src/test/java/sorting/SortAlgorithmTest.java
+++ src/test/java/sorting/SortAlgorithmTest.java
@@ -1,12 +1,8 @@
 package sorting;
 
-import java.util.Arrays;
-
-import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
 
-public abstract class SortAlgorithmTest {
+public abstract class SortAlgorithmTest extends AbstractSortAlgorithmTest {
 
     private SortAlgorithm sortAlgorithm;
 
@@ -15,75 +11,10 @@ public abstract class SortAlgorithmTest {
         sortAlgorithm = createSortingAlgorithm();
     }
 
-    protected abstract SortAlgorithm createSortingAlgorithm();
+    abstract SortAlgorithm createSortingAlgorithm();
 
-    @Test
-    public void testEmptySort() {
-        int[] expecteds = new int[0];
-        int[] actuals = new int[0];
-        actuals = sortAlgorithm.sort(actuals);
-        Assert.assertArrayEquals(expecteds, actuals);
-    }
-
-    @Test
-    public void testSortOnSingleElement() {
-        int[] expecteds = new int[] { 1 };
-        int[] actuals = new int[] { 1 };
-        actuals = sortAlgorithm.sort(actuals);
-        Assert.assertArrayEquals(expecteds, actuals);
-    }
-
-    @Test
-    public void testSortOnTwosortedElements() {
-        int[] expecteds = new int[] { 1, 2 };
-        int[] actuals = new int[] { 1, 2 };
-        actuals = sortAlgorithm.sort(actuals);
-        Assert.assertArrayEquals(expecteds, actuals);
-    }
-
-    @Test
-    public void testSortOnTwoUnsortedElements() {
-        int[] expecteds = new int[] { 1, 2 };
-        int[] actuals = new int[] { 2, 1 };
-        actuals = sortAlgorithm.sort(actuals);
-        Assert.assertArrayEquals(expecteds, actuals);
-    }
-
-    @Test
-    public void testSortOnManyElements1() {
-        int[] expecteds = new int[] { 1, 2, 3, 3, 4, 5, 8, 11 };
-        int[] actuals = new int[] { 3, 2, 11, 5, 1, 8, 3, 4 };
-        actuals = sortAlgorithm.sort(actuals);
-        Assert.assertArrayEquals(expecteds, actuals);
-    }
-
-    @Test
-    public void testSortOnManyElements2() {
-        int[] expecteds = new int[] { 1, 2, 3, 4, 5, 5, 7, 8, 9 };
-        int[] actuals = new int[] { 3, 7, 8, 5, 2, 1, 9, 5, 4 };
-        actuals = sortAlgorithm.sort(actuals);
-        Assert.assertArrayEquals(expecteds, actuals);
-    }
-
-    @Test
-    public void testSortOnManyElements3() {
-        int size = 10000;
-        int[] expecteds = new int[size];
-        for (int i = 0; i < size; i++) {
-            int value = (int) (Math.random() * Integer.MAX_VALUE * (Math.random() < 0.5 ? -1d : 1d));
-            expecteds[i] = value;
-        }
-        int[] actuals = Arrays.copyOf(expecteds, expecteds.length);
-        Arrays.sort(expecteds);
-        actuals = sortAlgorithm.sort(actuals);
-        Assert.assertArrayEquals(expecteds, actuals);
-    }
-
-    @Test
-    public void testSortOnManyElements4() {
-        int[] expecteds = new int[] { 2, 4, 5, 6, 7, 8, 9, 11, 12, 13, 19, 21 };
-        int[] actuals = new int[] { 13, 19, 9, 5, 12, 8, 7, 4, 11, 2, 6, 21 };
-        actuals = sortAlgorithm.sort(actuals);
-        Assert.assertArrayEquals(expecteds, actuals);
+    @Override
+    int[] sort(int[] actuals) {
+        return sortAlgorithm.sort(actuals);
     }
 }
