d34e235bcc08f0e8fb8da26146e165b698111d28
Loic Rouchon <loic.rouchon@gmail.com>
2013-06-22 16:50:01 +0200

    Euler problem 8: remove commented code

--- src/main/java/project/euler/Problem8.java
+++ src/main/java/project/euler/Problem8.java
@@ -17,35 +17,4 @@ public class Problem8 {
 		}
 		return maxProduct;
 	}
-	// public static int max5ConsecutiveDigitsProduct(String digitsString) {
-	// int maxProduct = 1;
-	// int product = 1;
-	// char[] digits = digitsString.toCharArray();
-	// Deque<Integer> previousDigits = new ArrayDeque<Integer>(5);
-	// for (int i = 0; i < Math.max(5, digits.length); i++) {
-	// int oldestDigit = 1;
-	// product = computeCurrentProduct(product, digits, i, oldestDigit,
-	// previousDigits);
-	// if (product > maxProduct) {
-	// maxProduct = product;
-	// }
-	// }
-	// for (int i = 5; i < digits.length; i++) {
-	// int oldestDigit = previousDigits.pop();
-	// product = computeCurrentProduct(product, digits, i, oldestDigit,
-	// previousDigits);
-	// if (product > maxProduct) {
-	// maxProduct = product;
-	// }
-	// }
-	// return maxProduct;
-	// }
-	//
-	// private static int computeCurrentProduct(int product, char[] digits, int
-	// i, int oldestDigit, Deque<Integer> previousDigits) {
-	// int digit = Integer.parseInt(Character.toString(digits[i]));
-	// previousDigits.addLast(digit);
-	// product = product / oldestDigit * digit;
-	// return product;
-	// }
 }
