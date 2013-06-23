341275ec7df666dcbf621efbcdf09620e29741ee
Loic Rouchon <loic.rouchon@gmail.com>
2013-06-23 17:04:24 +0200

    Add tests and comments for TrainingProblem1

--- src/main/java/problem/TrainingProblem1.java
+++ src/main/java/problem/TrainingProblem1.java
@@ -6,40 +6,49 @@ import java.util.List;
 import java.util.Set;
 
 public class TrainingProblem1 {
+	/**
+	 * Write a function f(a, b) which takes two character string arguments and
+	 * returns a string containing only the characters found in both strings in
+	 * the order of a. O(N²).
+	 */
+	public String commonCharsNSquared(String strA, String strB) {
+		List<Character> charactersList = new ArrayList<Character>(strA.length());
+		Set<Character> characters = new HashSet<Character>(strA.length());
+		for (char c : strA.toCharArray()) {
+			if (!characters.contains(c) && strB.indexOf(c) >= 0) {
+				characters.add(c);
+				charactersList.add(c);
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		for (Character c : charactersList) {
+			sb.append(c);
+		}
+		return sb.toString();
+	}
 
-    public String commonCharsNSquared(String strA, String strB) {
-        List<Character> charactersList = new ArrayList<Character>(strA.length());
-        Set<Character> characters = new HashSet<Character>(strA.length());
-        for (char c : strA.toCharArray()) {
-            if (!characters.contains(c) && strB.indexOf(c) >= 0) {
-                characters.add(c);
-                charactersList.add(c);
-            }
-        }
-        StringBuilder sb = new StringBuilder();
-        for (Character c : charactersList) {
-            sb.append(c);
-        }
-        return sb.toString();
-    }
-
-    public String commonCharsN(String strA, String strB) {
-        List<Character> charactersList = new ArrayList<Character>(strA.length());
-        Set<Character> characters = new HashSet<Character>(strA.length());
-        Set<Character> charactersB = new HashSet<Character>(strB.length());
-        for (char c : strB.toCharArray()) {
-            charactersB.add(c);
-        }
-        for (char c : strA.toCharArray()) {
-            if (charactersB.contains(c) && !characters.contains(c)) {
-                characters.add(c);
-                charactersList.add(c);
-            }
-        }
-        StringBuilder sb = new StringBuilder();
-        for (Character c : charactersList) {
-            sb.append(c);
-        }
-        return sb.toString();
-    }
+	/**
+	 * Write a function f(a, b) which takes two character string arguments and
+	 * returns a string containing only the characters found in both strings in
+	 * the order of a. O(N).
+	 */
+	public String commonCharsN(String strA, String strB) {
+		List<Character> charactersList = new ArrayList<Character>(strA.length());
+		Set<Character> characters = new HashSet<Character>(strA.length());
+		Set<Character> charactersB = new HashSet<Character>(strB.length());
+		for (char c : strB.toCharArray()) {
+			charactersB.add(c);
+		}
+		for (char c : strA.toCharArray()) {
+			if (charactersB.contains(c) && !characters.contains(c)) {
+				characters.add(c);
+				charactersList.add(c);
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		for (Character c : charactersList) {
+			sb.append(c);
+		}
+		return sb.toString();
+	}
 }
--- src/test/java/problem/TrainingProblem1Test.java
+++ src/test/java/problem/TrainingProblem1Test.java
@@ -7,29 +7,30 @@ import org.junit.Test;
 
 public class TrainingProblem1Test {
 
-    private TrainingProblem1 trainingProblem1;
+	private TrainingProblem1 trainingProblem1;
 
-    @Before
-    public void setUp() {
-        trainingProblem1 = new TrainingProblem1();
-    }
+	@Before
+	public void setUp() {
+		trainingProblem1 = new TrainingProblem1();
+	}
 
-    @Test
-    public void testCommonChars() {
-        testCommonChars("", "", "");
-        testCommonChars("", "a", "");
-        testCommonChars("", "", "a");
-        testCommonChars("a", "a", "a");
-        testCommonChars("a", "a", "ab");
-        testCommonChars("a", "ab", "a");
-        testCommonChars("ab", "ab", "ab");
-        testCommonChars("ab", "ab", "ba");
-        testCommonChars("ba", "ba", "ab");
-        testCommonChars("ba", "ba", "ba");
-    }
+	@Test
+	public void testCommonChars() {
+		testCommonChars("", "", "");
+		testCommonChars("", "a", "");
+		testCommonChars("", "", "a");
+		testCommonChars("a", "a", "a");
+		testCommonChars("a", "a", "ab");
+		testCommonChars("a", "ab", "a");
+		testCommonChars("ab", "ab", "ab");
+		testCommonChars("ab", "ab", "ba");
+		testCommonChars("ba", "ba", "ab");
+		testCommonChars("ba", "ba", "ba");
+		testCommonChars("Heo wr", "Hello world", "How are you");
+	}
 
-    private void testCommonChars(String expected, String a, String b) {
-        assertEquals(expected, trainingProblem1.commonCharsNSquared(a, b));
-        assertEquals(expected, trainingProblem1.commonCharsN(a, b));
-    }
+	private void testCommonChars(String expected, String a, String b) {
+		assertEquals(expected, trainingProblem1.commonCharsNSquared(a, b));
+		assertEquals(expected, trainingProblem1.commonCharsN(a, b));
+	}
 }
